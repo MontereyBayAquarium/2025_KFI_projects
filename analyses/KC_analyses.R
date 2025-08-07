49a536edf86bfca2fef474eca6c995a069240ac7
Joshua Smith <joshsmith@nceas.ucsb.edu>
2025-08-07 11:07:54 -0700

    working

--- analyses/KC_analyses.R
+++ analyses/KC_analyses.R
@@ -9,7 +9,7 @@ rm(list=ls())
 #Load packages, data, set dir
 
 require(librarian)
-librarian::shelf(tidyverse,ggplot2)
+librarian::shelf(tidyverse,ggplot2, janitor, readxl, googlesheets4)
 
 derm_raw <- read_sheet("https://docs.google.com/spreadsheets/d/1i9rHc8EAjMcqUqUDwjHtGhytUdG49VTSG9vfKmDPerQ/edit?gid=0#gid=0",
                        sheet = 4) %>% clean_names()
@@ -18,7 +18,7 @@ derm_raw <- read_sheet("https://docs.google.com/spreadsheets/d/1i9rHc8EAjMcqUqUD
 #Load packages, data, set dir
 
 # Step 1: prep data
-diet_overall <- derm_qc %>%
+diet_overall <- derm_raw %>%
   filter(!is.na(diet), diet != "None") %>%
   count(diet) %>%
   arrange(desc(n)) %>%
@@ -33,7 +33,7 @@ diet_overall <- derm_qc %>%
 ################################################################################
 #Plot size fq
 
-ggplot(diet_overall) +
+p <- ggplot(diet_overall) +
   geom_rect(aes(xmin = xmin, xmax = xmax, ymin = 0, ymax = 1, fill = diet), color = "white") +
   geom_text(
     data = diet_overall %>% filter(prop > 0.04),
@@ -43,7 +43,7 @@ ggplot(diet_overall) +
   scale_x_continuous(labels = scales::percent_format(accuracy = 1)) +
   scale_fill_brewer(palette = "Set2") +  # ✨ Try "Set2", "Dark2", or "Paired"
   labs(
-    title = "",
+    title = "Diet composition of actively foraging Dermasterias",
     x = "Proportion",
     y = NULL,
     fill = "Diet"
@@ -52,5 +52,24 @@ ggplot(diet_overall) +
   theme(
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank(),
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    plot.title = element_text(size=10),
+    legend.text = element_text(size=7)
+    
   )
+
+
+
+
+# Export
+ggsave(
+  filename = here::here("~", "Downloads", "diet_composition_plot.png"),
+  plot = p,  
+  width = 6,
+  height = 2,
+  dpi = 600,
+  bg = "white"
+)
+
+
+
