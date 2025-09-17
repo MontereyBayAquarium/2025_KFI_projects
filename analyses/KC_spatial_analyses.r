570f22ae274ee5fd5adc964c9aa5c1d1ec7b9306
Keenan Chau-Pech <keenanchau-pech@college10-169-233-117-136.resnet.ucsc.edu>
2025-09-17 15:10:23 -0700

    made a few simple figures

--- analyses/KC_spatial_analyses.r
+++ analyses/KC_spatial_analyses.r
@@ -4,6 +4,15 @@
 
 rm(list=ls())
 
+# Research question -------------------------------------------------------
+
+# Where in space are Dermasterias eating urchins?
+
+# Hypothesis --------------------------------------------------------------
+
+# Urchin predation will be most common in incipient 
+# forests in comparison to persistent forests and barrens
+
 # Load packages, data, set dir --------------------------------------------
 
 require(librarian)
@@ -27,6 +36,112 @@ derm_margin_spatial <- margin_derm_raw %>% dplyr::select(site, heading_out, tran
                                                          depth_start, depth_end, segment, 
                                                          species, size, count, 
                                                          diet, urchin_size)
+
+# Wrangling (recovery) ----------------------------------------------------
+
+# filter occurrence data for site type
+derm_site_type <- derm_recovery_spatial %>% 
+  select(site_type)
+
+view(derm_site_type)
+
+# filter + mutate for sizes at different depths
+derm_depth_size <- derm_recovery_spatial %>% 
+  select(depth, size)   
+
+view(derm_depth_size)
+
+# filter for site-specific predation, remove none's
+
+derm_site_pred <- derm_recovery_spatial %>% 
+  select(site_type, diet) %>% 
+  filter(!(diet %in% 
+             c("None")))
+
+view(derm_site_pred)
+
+# filter for just urchin eaters and site type
+
+derm_site_urchin <- filter(derm_recovery_spatial, 
+                           diet == "Urchin")
+
+# Visualization (recovery) ------------------------------------------------
+
+# site occurrence bar plot
+ggplot(derm_site_type, 
+       aes(x = site_type)) + 
+  geom_bar(show.legend = "FALSE") + 
+  theme_minimal()
+
+# size at depth scatterplot
+
+ggplot(derm_depth_size, 
+       aes(x = depth, 
+           y = size)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  theme_minimal()
+
+# Bar plot of site-specific predation
+
+ggplot(derm_site_pred, 
+       aes(x = diet, 
+           fill = site_type)) + 
+  geom_bar(show.legend = FALSE) + 
+  facet_wrap(~site_type, 
+             ncol = 1) + 
+  scale_fill_brewer(palette = "Set2") + 
+  theme_minimal()
+
+# Bar plot of site-specific urchin predation
+
+ggplot(derm_site_urchin, 
+       aes(x = site_type, 
+           fill = site_type)) + 
+  geom_bar(show.legend = FALSE) + 
+  scale_fill_brewer(palette = "Set2") + 
+  theme_minimal()
+
+# Final figures (recovery) ------------------------------------------------
+
+
+
+
+
+# Asking R to do math for me (nicely) -------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
